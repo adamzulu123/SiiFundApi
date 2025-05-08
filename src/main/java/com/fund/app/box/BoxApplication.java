7dd0a9e2850aefe85cb2123bbd960dd11523cceb
adamzulu123 <120639376+adamzulu123@users.noreply.github.com>
2025-05-09 00:47:22 +0200

    Initial commit

---
+++ src/main/java/com/fund/app/box/BoxApplication.java
@@ -0,0 +1,13 @@
+package com.fund.app.box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxApplication.class, args);
+	}
+
+}
